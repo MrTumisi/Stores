bf83fe686102c3163c5464be847e0e326329564e
MrTumisi <petrustumisisabata2076@gmail.com>
2022-01-19 12:45:20 +0200

    Initial setup

---
+++ Stores.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Stores.UI.Startup))]
+namespace Stores.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
