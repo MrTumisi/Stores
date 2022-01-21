9c64c21cc5c5ede5867dbc88cec149b60140bf80
MrTumisi <petrustumisisabata2076@gmail.com>
2022-01-21 11:44:39 +0200

    Product Controller

---
+++ Stores.UI/Controllers/ProductController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Stores.Core.Models;
+using Stores.DataAccess.InMemory;
+
+namespace Stores.UI.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductRepository context;
+        public ProductController()
+        {
+            context = new ProductRepository();
+        }
+        // GET: Product
+        public ActionResult Index()
+        {
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(product);
+            }
+            else
+            {
+                context.Insert(product);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult Edit(Product product, string Id)
+        {
+            Product prod = context.Find(Id);
+            if (prod == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(product);
+                }
+                prod.Description = product.Description;
+                prod.Name = product.Name;
+                prod.Price = product.Price;
+                prod.Category = product.Category;
+                prod.Image = product.Image;
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
